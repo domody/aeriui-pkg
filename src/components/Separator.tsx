f26eff16e0d7392c7b9d4fb9657775237225bf42
Dom Ody <dom20.ody07@gmail.com>
2025-05-09 02:56:13 +0200

    Separator rename and ESLint fixes

---
+++ src/components/Separator.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { cn } from "@/app/lib/utils/cn";
+
+interface SeparatorProps extends React.HTMLAttributes<HTMLDivElement> {
+  vertical?: boolean;
+}
+
+const Separator = React.forwardRef<HTMLDivElement, SeparatorProps>(
+  ({ className, children, vertical = false, ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className={cn(
+          `flex items-center ${vertical ? "h-full flex-col" : "w-full"}`,
+          className
+        )}
+      >
+        <div
+          className={`bg-border ${vertical ? "h-full w-px" : "h-px w-full"}`}
+          {...props}
+        />
+        {children && (
+          <>
+            <p
+              className={`text-border text-sm text-nowrap ${
+                vertical ? "my-4" : "mx-4"
+              }`}
+            >
+              {children}
+            </p>
+            <div
+              className={`bg-border ${
+                vertical ? "h-full w-px" : "h-px w-full"
+              }`}
+              {...props}
+            />
+          </>
+        )}
+      </div>
+    );
+  }
+);
+Separator.displayName = "Separator";
+
+export { Separator };
